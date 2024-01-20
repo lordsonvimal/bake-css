a54b51cffd92f0dc7b344ae7e1708000b6f97611
Lordson Vimal <ylordson@gmail.com>
2024-01-20 22:41:55 +0530

    [WIP] Layout system

---
+++ src/layouts/container.ts
@@ -0,0 +1,5 @@
+// Container is a flex
+// Can be a row based / column based
+// Container can have another container / an item
+// Can have all container related flex properties. See: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+// See 8pt grid system: https://uxplanet.org/everything-you-should-know-about-8-point-grid-system-in-ux-design-b69cb945b18d
---
+++ src/layouts/item.ts
@@ -0,0 +1,3 @@
+// Item is a child of a container
+// Item can have properties related to a flex child
+// Does not scroll. To scroll, a container is needed
