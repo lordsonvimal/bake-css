d1fb181a54ff33415a095b1d42431d6df4e3554d
Lordson Vimal <ylordson@gmail.com>
2024-01-21 22:30:13 +0530

    [WIP] Token

---
+++ src/base/token.ts
@@ -0,0 +1,225 @@
+// All the global tokens goes here
+
+export const GLOBAL_BORDER = "0px";
+
+export const BORDERS = {
+  1: "1px",
+  3: "3px",
+  5: "5px",
+  "50p": "50%"
+};
+
+export const SPACES = {
+  4: "4px",
+  8: "8px",
+  16: "16px",
+  24: "24px",
+  32: "32px",
+  auto: "auto",
+  unset: "unset"
+};
+
+export const PADDING_PROPS = {
+  all: "p",
+  inline: "pi",
+  left: "pl",
+  right: "pr",
+  top: "pt",
+  bottom: "pb"
+};
+
+export const MARGIN_PROPS = {
+  all: "m",
+  inline: "mi",
+  left: "ml",
+  right: "mr",
+  top: "mt",
+  bottom: "mb"
+};
+
+export const BORDER_PROPS = {
+  left: "bl",
+  top: "bt",
+}
+
+const ALIGN_PROPS_NAME = "a";
+const ANIMATION_PROPS_NAME = "an";
+const BACKFACE_PROPS_NAME = "bf";
+const BACKGROUND_PROPS_NAME = "bg";
+const BORDER_PROPS_NAME = "b";
+const BOTTOM_PROPS_NAME = "bot";
+const BOX_PROPS_NAME = "box";
+const CAPTION_SIDE_PROPS_NAME = "cs";
+const CLEAR_PROPS_NAME = "cl";
+const CLIP_PROPS_NAME = "cp";
+const COLOR_PROPS_NAME = "c";
+const COLUMN_PROPS_NAME = "col";
+
+// const ALIGN_CONTENT_NAME = "align-content";
+// const ALIGN_ITEMS_NAME = "align-items";
+// const ALIGN_ITEMS_NAME = "align-items";
+// align-self
+// animation
+// animation-delay
+// animation-direction
+// animation-duration
+// animation-fill-mode
+// animation-iteration-count
+// animation-name
+// animation-play-state
+// animation-timing-function
+// backface-visibility
+// background
+// background-attachment
+// background-clip
+// background-color
+// background-image
+// background-origin
+// background-position
+// background-repeat
+// background-size
+// border
+// border-bottom
+// border-bottom-color
+// border-bottom-left-radius
+// border-bottom-right-radius
+// border-bottom-style
+// border-bottom-width
+// border-collapse
+// border-color
+// border-image
+// border-image-outset
+// border-image-repeat
+// border-image-slice
+// border-image-source
+// border-image-width
+// border-left
+// border-left-color
+// border-left-style
+// border-left-width
+// border-radius
+// border-right
+// border-right-color
+// border-right-style
+// border-right-width
+// border-spacing
+// border-style
+// border-top
+// border-top-color
+// border-top-left-radius
+// border-top-right-radius
+// border-top-style
+// border-top-width
+// border-width
+// bottom
+// box-shadow
+// box-sizing
+// caption-side
+// clear
+// clip
+// color
+// column-count
+// column-fill
+// column-gap
+// column-rule
+// column-rule-color
+// column-rule-style
+// column-rule-width
+// column-span
+// column-width
+// columns
+// content
+// counter-increment
+// counter-reset
+// cursor
+// direction
+// display
+// empty-cells
+// flex
+// flex-basis
+// flex-direction
+// flex-flow
+// flex-grow
+// flex-shrink
+// flex-wrap
+// float
+// font
+// font-family
+// font-size
+// font-size-adjust
+// font-stretch
+// font-style
+// font-variant
+// font-weight
+// height
+// justify-content
+// left
+// letter-spacing
+// line-height
+// list-style
+// list-style-image
+// list-style-position
+// list-style-type
+// margin
+// margin-bottom
+// margin-left
+// margin-right
+// margin-top
+// max-height
+// max-width
+// min-height
+// min-width
+// opacity
+// order
+// outline
+// outline-color
+// outline-offset
+// outline-style
+// outline-width
+// overflow
+// overflow-x
+// overflow-y
+// padding
+// padding-bottom
+// padding-left
+// padding-right
+// padding-top
+// page-break-after
+// page-break-before
+// page-break-inside
+// perspective
+// perspective-origin
+// position
+// quotes
+// resize
+// right
+// tab-size
+// table-layout
+// text-align
+// text-align-last
+// text-decoration
+// text-decoration-color
+// text-decoration-line
+// text-decoration-style
+// text-indent
+// text-justify
+// text-overflow
+// text-shadow
+// text-transform
+// top
+// transform
+// transform-origin
+// transform-style
+// transition
+// transition-delay
+// transition-duration
+// transition-property
+// transition-timing-function
+// vertical-align
+// visibility
+// white-space
+// width
+// word-break
+// word-spacing
+// word-wrap
+// z-index
